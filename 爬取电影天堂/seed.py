b1885ca9afabad14cebb71f17d83e44d87d65830
Tactful-biao <1208339113@qq.com>
2018-03-17 17:45:20 +0800

    更新了脚本

--- 爬取电影天堂/seed.py
+++ 爬取电影天堂/seed.py
@@ -1,31 +1,35 @@
-#!/usr/bin/env python3
-# encoding=utf-8
-
-## 时间：2017-9-17 ##
-## 博客地址：http://bbiao.me ##
-## 该代码所在文章： http://bbiao.me/爬取《电影天堂》所有电影种子  ##
-
-
-__author__'sunshibiao'
-
-
 import requests
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
 
-links = []
-with open('电影种子', 'w+') as fp:
-    for link in range(1, 11471):
-        try:
-            rel_url = 'http://www.btbtdy.com/down/' + str(link) + '-0-0.html'
-            html = requests.get(rel_url).content
-            soup = BeautifulSoup(html, 'lxml')
-            title = soup.find('h1').getText()
-            downlist = soup.find('form', attrs={'name': 'form2', 'id': 'form2'}).find('input')['value']
-            data = {
-                '电影名称:': title,
-                '种子链接:': downlist
-            }
-            fp.write(str(data)+'\n')
-            print(data)
-        except:
-            pass
+def get_seed(i):
+    url = 'http://www.btbtdy.com/downlist/' + str(i) + '-0-0.html'
+    headers = {
+        'Host': 'www.btbtdy.com',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+    }
+    try:
+        html = requests.get(url, headers=headers).text
+        soup = BeautifulSoup(html, 'lxml')
+        title = soup.find_all('p')[0].getText()
+        link = soup.find_all('p')[1].getText()
+        data = {
+            '电影名称:': title,
+            '种子链接:': link
+        }
+        print(data)
+        with open('seed.txt', 'a+', encoding='utf-8') as sd:
+            sd.write(str(data) + '\n')
+    except:
+        with open('error.txt', 'a+', encoding='utf-8') as e:
+            e.write('遇到异常，链接的id是: ' + str(i) + '\n')
+        pass
+
+if __name__ == '__main__':
+    pool = Pool(10)
+    page = [x for x in range(1, 12411)]
+    pool.map(get_seed, page)
+    pool.close()
+    pool.join()
+
