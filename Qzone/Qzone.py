723e0bec6db50972047f577c7a26a1b871a4df58
Shibiao Sun <1208339113@qq.com>
2019-08-15 12:16:47 +0800

    更新代码
    针对空间最新的反爬虫策略进行了代码更改。

--- Qzone/Qzone.py
+++ Qzone/Qzone.py
@@ -34,13 +34,19 @@ class Spider(object):
         登录、调用get_g_tk()、get_friends()函数
         :return:
         '''
+        self.driver.set_window_position(20, 40)
+        self.driver.set_window_size(1100, 700)
         self.driver.switch_to.frame('login_frame')
+        self.driver.execute_script("var a = document.getElementById('qlogin_list');a.removeChild(a.firstElementChild);")
         self.driver.find_element_by_id('switcher_plogin').click()
         self.driver.find_element_by_id('u').clear()
         self.driver.find_element_by_id('u').send_keys(self.__username)
+        time.sleep(2)
         self.driver.find_element_by_id('p').clear()
         self.driver.find_element_by_id('p').send_keys(self.__password)
+        time.sleep(2)
         self.driver.find_element_by_id('login_button').click()
+        time.sleep(3)
         self.driver.get('http://user.qzone.qq.com/{}'.format(self.__username))
         cookie = ''
         for item in self.driver.get_cookies():
@@ -56,18 +62,27 @@ class Spider(object):
         获取全部好友
         :return: qq, name
         '''
-        url = 'https://user.qzone.qq.com/proxy/domain/r.qzone.qq.com/cgi-bin/tfriend/friend_hat_get.cgi?'
+        url = 'https://h5.qzone.qq.com/proxy/domain/base.qzone.qq.com/cgi-bin/right/get_entryuinlist.cgi?'
         params = {
             'uin': self.__username,
             'fupdate': 1,
+            'action': 1,
             'g_tk': self.g_tk
         }
-        url = url + parse.urlencode(params)
-        friends = self.req.get(url, headers=self.headers).text
+        _url = url + parse.urlencode(params)
+        offset = 0
         name, qq_num = [], []
-        for _qq, _name in zip(re.findall('"\d+"', friends), re.findall('"realname":.*"', friends)):
-            name.append(re.sub('"|realname|:', '', _name))
-            qq_num.append(re.sub('"', '', _qq))
+        while True:
+            url = _url + '&offset=' + str(offset)
+            friends = self.req.get(url, headers=self.headers).text
+            if '\"end\":1' not in friends and '\"uinlist\":[]' not in friends:
+                for _qq, _name in zip(re.findall('"data":"(\d+)"', friends), re.findall('"label":"(.*?)"', friends)):
+                    name.append(_name)
+                    qq_num.append(_qq)
+            else:
+                break
+            offset += 50
+
         self.name, self.qq_num = name, qq_num
 
     def get_g_tk(self):
@@ -123,53 +138,29 @@ class Spider(object):
                         }
                         black.insert(data)
                 else:
-                    created_time = re.findall('created_time":\d+', mood.text)
-                    source = re.findall('source_appid":".*?"source_name":".*?"', mood.text)
-                    contents = re.findall('],"content":".*?"', mood.text)
-                    forword = re.findall('fwdnum":\d+', mood.text)
+                    created_time = re.findall('created_time":(\d+)', mood.text)
+                    source = re.findall('source_appid":".*?"source_name":"(.*?)"', mood.text)
+                    contents = re.findall('],"content":"(.*?)"', mood.text)
+                    forword = re.findall('fwdnum":(\d+)', mood.text)
                     comment_content = re.findall('commentlist":(null|.*?],)', mood.text)
-                    comments = re.findall('cmtnum":\d+', mood.text)
+                    comments = re.findall('cmtnum":(\d+)', mood.text)
                     pics = re.findall('","pic(_template|".*?])', mood.text)
-                    like_url = 'https://user.qzone.qq.com/proxy/domain/users.qzone.qq.com/cgi-bin/likes/get_like_list_app?'
-                    tids = re.findall('tid":".*?"', mood.text)
+                    tids = re.findall('tid":"(.*?)"', mood.text)
 
                     for _time, _source, _content, _forword, _comment_content, _comment, _pic, _tid in \
                             zip(created_time, source, contents, forword, comment_content, comments, pics, tids):
-                        param = {
-                            'uin': self.__username,
-                            'unikey': 'http://user.qzone.qq.com/{}/mood/'.format(q)+re.sub('tid":"|"', '', _tid)+'.1',
-                            'begin_uin': 0,
-                            'query_count': 60,
-                            'if_first_page': 1,
-                            'g_tk': self.g_tk
-                        }
-                        like_url = like_url + parse.urlencode(param)
-                        like = self.req.get(url=like_url, headers=self.headers)
-                        likers = like.text.encode(like.encoding).decode('utf-8')
-                        fuin, nick, sex, constellation, address = re.findall('fuin":\d+', likers), re.findall('nick":".*?"', likers), re.findall('gender":".*?"', likers), re.findall('tion":".*?"', likers), re.findall('addr":".*?"', likers)
-                        infos = []
-                        for _fuin, _nick, _sex, _constellation, _address in zip(fuin, nick, sex, constellation, address):
-                            info = {
-                                'fuin': re.sub('fuin":', '', _fuin),
-                                'nick': re.sub('nick":"|"', '', _nick),
-                                'sex': re.sub('gender":"|"', '', _sex),
-                                'constellation': re.sub('tion":"|"', '', _constellation),
-                                'address': re.sub('addr":"|"', '', _address)
-                            }
-                            infos.append(info)
                         data = {
                             '_id': str(q) + '_' + str(random.random() * 10).replace('.', ''),
                             'name': self.name[self.qq_num.index(q)],
-                            'CreateTime': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(re.sub('created_time":', '', _time)))),
-                            'source': re.sub('source_appid":".*?"source_name":"|"', '', _source),
-                            'content': re.sub('],"content":"|"', '', _content),
-                            'forward': re.sub('fwdnum":', '', _forword),
-                            'comment_content': re.sub('null|commentlist":', '', _comment_content) if 'null' in _comment_content else str([(re.sub('content":"|"', '', x), re.sub('createTime2":"|"', '', y), re.sub('name":"|"', '', z), re.sub('uin":', '', zz)) for x, y, z, zz in zip(re.findall('content":".*?"', _comment_content), re.findall('createTime2":".*?"', _comment_content), re.findall('name":".*?"', _comment_content), re.findall('uin":\d+', _comment_content))]),
-                            'comment': re.sub('cmtnum":', '', _comment),
-                            'pic': [] if 'template' in _pic else [re.sub('url2":|"', '', i) for i in re.findall('url2":".*?"', _pic)],
-                            'like': re.sub('number":', '', re.search('number":\d+', likers).group()),
-                            'likers': infos
+                            'CreateTime': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(_time))),
+                            'source': _source,
+                            'content': _content,
+                            'forward': _forword,
+                            'comment_content': re.sub('null|commentlist":', '', _comment_content) if 'null' in _comment_content else str([(x, y, z, zz) for x, y, z, zz in zip(re.findall('content":"(.*?)"', _comment_content), re.findall('createTime2":"(.*?)"', _comment_content), re.findall('name":"(.*?)"', _comment_content), re.findall('uin":(\d+)', _comment_content))]),
+                            'comment': _comment,
+                            'pic': [] if 'template' in _pic else [i for i in re.findall('url2":"(.*?)"', _pic)],
                         }
+
                         if shuoshuo.insert(data):
                             print('%s 的说说写入到数据库成功！' % self.name[self.qq_num.index(q)])
                         else:
@@ -209,18 +200,18 @@ class Spider(object):
                 else:
                     text = board.text
                     ids, nickname, uin, pubtime, content, replyList = \
-                        re.findall('id":"\d+', text), re.findall('nickname":".*?"', text), re.findall('uin":\d+,\n"nick', text),\
-                        re.findall('pubtime":".*?"', text), re.findall('ubbContent":".*?"', text), re.findall('"replyList":(\[\]|.*?\}\])', text, re.S)
+                        re.findall('id":"(\d+)', text), re.findall('nickname":"(.*?)"', text), re.findall('uin":(\d+),\n"nick', text),\
+                        re.findall('pubtime":"(.*?)"', text), re.findall('ubbContent":"(.*?)"', text), re.findall('"replyList":(\[\]|.*?\}\])', text, re.S)
                     for _id, _nickname, _uin, _time, _content, _reply in zip(ids, nickname, uin, pubtime, content, replyList):
                         data = {
-                            '_id': str(q) + '_' + re.sub('id":"', '', _id),
+                            '_id': str(q) + '_' + _id,
                             'owner': self.name[self.qq_num.index(q)],
-                            'total': re.sub('total":', '', re.search('total":\d+', board.text).group()),
-                            'name': re.sub('nickname":"|"', '', _nickname),
-                            'qq': re.sub('uin":|,\n"nick', '', _uin),
-                            'time': re.sub('pubtime":"|"', '', _time),
-                            'content': re.sub('ubbContent":"|"', '', _content),  # 下行需要改动
-                            'replyList': [] if '[]' in _reply else str([re.sub('nick":"|"', '', name) + re.sub('content"|"', '', con) for name, con in zip(re.findall('nick":".*?"', _reply), re.findall('content":".*?"', _reply))])
+                            'total': re.search('total":(\d+)', board.text).group(1),
+                            'name': _nickname,
+                            'qq': _uin,
+                            'time': _time,
+                            'content': _content,  # 下行需要改动
+                            'replyList': [] if '[]' in _reply else str([name + re.sub('\[em\]|e.*?\em\]', '', con) for name, con in zip(re.findall('nick":"(.*?)"', _reply), re.findall('content":"(.*?)"', _reply))])
                         }
                         if boardb.insert(data):
                             print('%s 的留言存储到Mongodb成功！' % self.name[self.qq_num.index(q)])
@@ -252,32 +243,31 @@ class Spider(object):
                     constellation = ['白羊座', '金牛座', '双子座', '巨蟹座', '狮子座', '处女座', '天秤座', '天蝎座', '射手座', '摩羯座', '水瓶座', '双鱼座', '未填写']
                     data = {
                         '_id': str(q) + '_' + str(random.random() * 10).replace('.', ''),
-                        'nickname': re.sub('nickname":"|"', '', re.search('nickname":".*?"', text).group()),
-                        'spacename': re.sub('spacename":"|"', '', re.search('spacename":".*?"', text).group()),
-                        'desc': re.sub('desc":"|"', '', re.search('desc":".*?"', text).group()),
-                        'signature': re.sub('signature":"|"', '', re.search('signature":".*?"', text).group()),
-                        'sex': sex[int(re.sub('sex":', '', re.search('sex":\d+', text).group()))],
-                        'age': re.sub('"age":', '', re.search('"age":\d+', text).group()),
-                        'birthday': re.sub('birthyear":', '', re.search('birthyear":\d+', text).group()) + '-' + re.sub('birthday":"|"', '', re.search('birthday":".*"', text).group()),
-                        'constellation': constellation[int(re.sub('constellation":|,', '', re.search('constellation":.*,', text).group()).replace('-1', '12'))],
-                        'country': re.sub('country":"|"', '', re.search('country":".*"', text).group()),
-                        'province': re.sub('province":"|"', '', re.search('province":".*?"', text).group()),
-                        'city': re.sub('city":"|"', '', re.search('city":".*?"', text).group()),
+                        'nickname': re.search('nickname":"(.*?)"', text).group(1),
+                        'spacename': re.search('spacename":"(.*?)"', text).group(1),
+                        'desc': re.search('desc":"(.*?)"', text).group(1),
+                        'signature': re.search('signature":"(.*?)"', text).group(1),
+                        'sex': sex[int(re.search('sex":(\d+)', text).group(1))],
+                        'age': re.search('"age":(\d+)', text).group(1),
+                        'birthday': re.search('birthyear":(\d+)', text).group(1) + '-' + re.search('birthday":"(.*)"', text).group(1),
+                        'constellation': constellation[int(re.search('constellation":(.*),', text).group(1).replace('-1', '12'))],
+                        'country': re.search('country":"(.*)"', text).group(1),
+                        'province': re.search('province":"(.*?)"', text).group(1),
+                        'city': re.search('city":"(.*?)"', text).group(1),
                         'hometown': re.sub('hco":"|"|,|\n|hc|hp|:', '', re.search('hco":".*\n".*\n".*', text).group()),
-                        'marriage': marriage[int(re.sub('marriage":', '', re.search('marriage":\d', text).group()))],
-                        'career': re.sub('career":"|"', '', re.search('career":".*?"', text).group()),
-                        'address': re.sub('cb":"|"', '', re.search('cb":".*?"', text).group())
+                        'marriage': marriage[int(re.search('marriage":(\d)', text).group(1))],
+                        'career': re.search('career":"(.*?)"', text).group(1),
+                        'address': re.search('cb":"(.*?)"', text).group(1)
                     }
                     if table.insert(data):
                         print('%s 的信息写入到数据库成功！' % self.name[self.qq_num.index(q)])
                     t3 = False
 
+
 if __name__ == '__main__':
     sp = Spider()
     sp.login()
     sp.get_information()
-    t = time.perf_counter()
     sp.get_board()
     sp.get_mood()
-    End = time.perf_counter() - t
-    print('所有内容爬取完成！总用时%s!' % End)
+    print('爬去完成')
